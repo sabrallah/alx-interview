8478853b07e26c072d58c8b54d46a13b4f661474
sabrallah <sabrallah1991@gmail.com>
2024-01-09 08:56:23 +0000

    lookbox test

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
-    """
-    Determine if all boxes can be unlocked.
+    """Determine if all boxes can be opened."""
+    number_of_boxes = len(boxes)
+    unlocked_boxes = [False] * number_of_boxes
+    unlocked_boxes[0] = True
+    stack = [0]
 
-    This function iterates over a list of lists (boxes), where each list
-    represents a box and contains keys to other boxes. It starts with the
-    first box unlocked (box 0) and iterates through the keys in each box.
-    If a key corresponds to a box that hasn't been unlocked yet, it's added
-    to the list of keys. If all boxes can be unlocked, the function returns
-    True; otherwise, it returns False.
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key < number_of_boxes and not unlocked_boxes[key]:
+                unlocked_boxes[key] = True
+                stack.append(key)
 
-    Parameters:
-    boxes (list of lists): Each list represents a
-    box and contains keys to other boxes.
-
-    Returns:
-    bool: True if all boxes can be unlocked, else False.
-    """
-    keys = [0]
-    for key in keys:
-        for box_key in boxes[key]:
-            if box_key not in keys and box_key < len(boxes):
-                keys.append(box_key)
-    if len(keys) == len(boxes):
-        return True
-    return False
+    return all(unlocked_boxes)
