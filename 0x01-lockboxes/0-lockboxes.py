38c2609ff1e68525104205b81efd522e434ae751
sabrallah <sabrallah1991@gmail.com>
2024-01-09 08:58:04 +0000

    lookbox test

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
+"""
+    Lockboxes problem solution
+"""
+
+
 def canUnlockAll(boxes):
-    """Determine if all boxes can be opened."""
-    number_of_boxes = len(boxes)
-    unlocked_boxes = [False] * number_of_boxes
-    unlocked_boxes[0] = True
-    stack = [0]
+    """
+        Returns a truthy value if all boxes in the array can opened
+    """
+    # check if boxes is empty
+    if len(boxes) == 0:
+        return False
 
-    while stack:
-        box = stack.pop()
-        for key in boxes[box]:
-            if key < number_of_boxes and not unlocked_boxes[key]:
-                unlocked_boxes[key] = True
-                stack.append(key)
+    unlocked_set = set()
+    unlocked_set.add(0)
 
-    return all(unlocked_boxes)
+    for index, val in enumerate(boxes):
+        # check if index box can't be unlocked
+        if index not in unlocked_set:
+            return False
+
+        # add key indexes from unlocked box
+        for elem in val:
+            # go to each elem index and update the set
+            if elem < len(boxes) and elem > index:
+                unlocked_set.update(boxes[elem])
+        unlocked_set.update(val)
+
+    return True
