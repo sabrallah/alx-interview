e30d8b3443a1516bdb07c4f691ed793d9d5022c8
sabrallah <sabrallah1991@gmail.com>
2024-01-23 11:08:04 +0000

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,42 @@
 #!/usr/bin/python3
-
 import sys
+import signal
+
+# Initialize variables
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+file_size = 0
 
 
-def print_statistics(total_size, status_count):
-    print(f"File size: {total_size}")
-    for code in sorted(status_count):
-        print(f"{code}: {status_count[code]}")
+def print_stats():
+    print("File size: {:d}".format(file_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{:s}: {:d}".format(code, status_codes[code]))
 
 
-def parse_line(line, total_size, status_count):
-    try:
-        parts = line.split()
-        size = int(parts[-1])
-        status_code = int(parts[-2])
+def signal_handler(sig, frame):
+    print_stats()
+    sys.exit(0)
 
-        total_size += size
-
-        if status_code in {200, 301, 400, 401, 403, 404, 405, 500}:
-            status_count[status_code] = status_count.get(status_code, 0) + 1
-
-        return total_size, status_count
-
-    except (ValueError, IndexError):
-        return total_size, status_count
+# Handle Ctrl+C
 
 
-def main():
-    total_size = 0
-    status_count = {}
-
-    try:
-        for i, line in enumerate(sys.stdin, 1):
-            total_size, status_count = parse_line(line.strip(),
-                                                  total_size, status_count)
-
-            if i % 10 == 0:
-                print_statistics(total_size, status_count)
-
-    except KeyboardInterrupt:
-        pass  # Handle keyboard interruption
+signal.signal(signal.SIGINT, signal_handler)
 
 
+# Main loop
 if __name__ == "__main__":
-    main()
+    for i, line in enumerate(sys.stdin, 1):
+        try:
+            parts = line.split()
+            status_code = parts[-2]
+            file_size += int(parts[-1])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except IndexError:
+            pass
+        if i % 10 == 0:
+            print_stats()
+
+    print_stats()
