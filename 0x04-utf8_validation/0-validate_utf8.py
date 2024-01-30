2b0a07e78020b0eef49f80235da3fd4669b85486
sabrallah youssef <58825588+sabrallah@users.noreply.github.com>
2024-01-30 21:13:23 +0000

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,76 +1,32 @@
-import sys
-
-def validUTF8(data):
-    num_trailing_bytes = 0
-
-    for num in data:
-        if num_trailing_bytes == 0:
-            if (num >> 7) == 0b0:
-                continue
-            elif (num >> 5) == 0b110:
-                num_trailing_bytes = 1
-            elif (num >> 4) == 0b1110:
-                num_trailing_bytes = 2
-            elif (num >> 3) == 0b11110:
-                num_trailing_bytes = 3
-            else:
-                return False
-        else:
-            if (num >> 6) == 0b10:
-                num_trailing_bytes -= 1
-            else:
-                return False
-
-    return num_trailing_bytes == 0
-
-
-if __name__ == "__main__":
-    data = sys.argv[1:]
-    data = [int(num) for num in data]
-    result = validUTF8(data)
-    print(result)
+#!/usr/bin/env python
 """
-Module for UTF-8 Validation
+This module provides a function to validate UTF-8 encoded data.
 """
 
 
 def validUTF8(data):
     """
-    Determines if a given data set represents a valid UTF-8 encoding.
+    Validates whether the given data is UTF-8 encoded.
+
     Args:
-        data (list): List of integers representing 1 byte of data.
+        data (list[int]): The data to be validated.
 
     Returns:
-        bool: True if data is a valid UTF-8 encoding, else return False.
+        bool: True if the data is valid UTF-8 encoded, False otherwise.
     """
-    # Number of trailing bytes for the current UTF-8 character
-    num_trailing_bytes = 0
-
+    num_bytes = 0
     for num in data:
-        # Check if the current byte is a trailing byte
-        if num_trailing_bytes == 0:
-            if (num >> 7) == 0b0:
-                # Single byte character
-                continue
-            elif (num >> 5) == 0b110:
-                # Two byte character
-                num_trailing_bytes = 1
+        if num_bytes == 0:
+            if (num >> 5) == 0b110:
+                num_bytes = 1
             elif (num >> 4) == 0b1110:
-                # Three byte character
-                num_trailing_bytes = 2
+                num_bytes = 2
             elif (num >> 3) == 0b11110:
-                # Four byte character
-                num_trailing_bytes = 3
-            else:
-                # Invalid start of UTF-8 character
+                num_bytes = 3
+            elif (num >> 7):
                 return False
         else:
-            # Check if the current byte is a trailing byte
-            if (num >> 6) == 0b10:
-                num_trailing_bytes -= 1
-            else:
-                # Invalid trailing byte
+            if (num >> 6) != 0b10:
                 return False
-
-    # Check if there are missing trailing bytes at the end
-    return num_trailing_bytes == 0
+            num_bytes -= 1
+    return num_bytes == 0
