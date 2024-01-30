ed8a13548e03390dd753741303fffcfcb57c3c62
sabrallah youssef <58825588+sabrallah@users.noreply.github.com>
2024-01-30 21:08:49 +0000

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,4 +1,34 @@
-#!/usr/bin/python3
+import sys
+
+def validUTF8(data):
+    num_trailing_bytes = 0
+
+    for num in data:
+        if num_trailing_bytes == 0:
+            if (num >> 7) == 0b0:
+                continue
+            elif (num >> 5) == 0b110:
+                num_trailing_bytes = 1
+            elif (num >> 4) == 0b1110:
+                num_trailing_bytes = 2
+            elif (num >> 3) == 0b11110:
+                num_trailing_bytes = 3
+            else:
+                return False
+        else:
+            if (num >> 6) == 0b10:
+                num_trailing_bytes -= 1
+            else:
+                return False
+
+    return num_trailing_bytes == 0
+
+
+if __name__ == "__main__":
+    data = sys.argv[1:]
+    data = [int(num) for num in data]
+    result = validUTF8(data)
+    print(result)
 """
 Module for UTF-8 Validation
 """
