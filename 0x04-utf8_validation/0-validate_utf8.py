3b1ff30e676101728c7380d8e8edb748727dcb4a
sabrallah youssef <58825588+sabrallah@users.noreply.github.com>
2024-01-30 21:04:52 +0000

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -44,17 +44,3 @@ def validUTF8(data):
 
     # Check if there are missing trailing bytes at the end
     return num_trailing_bytes == 0
-
-# Test cases
-
-
-if __name__ == "__main__":
-    data1 = [65]
-    print(validUTF8(data1))  # True
-
-    data2 = [80, 121, 116, 104, 111, 110, 32, 105,
-             115, 32, 99, 111, 111, 108, 33]
-    print(validUTF8(data2))  # True
-
-    data3 = [229, 65, 127, 256]
-    print(validUTF8(data3))  # False
