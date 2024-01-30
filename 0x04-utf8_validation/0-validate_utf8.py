a1a44042e403bde686875a45abcde9cdb2d3e97f
sabrallah youssef <58825588+sabrallah@users.noreply.github.com>
2024-01-30 20:57:42 +0000

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,37 +1,41 @@
+#!/usr/bin/python3
 """
-This module contains a function for validating UTF-8 encoded data.
+Module for validating UTF-8 encoding
 """
 
 
 def validUTF8(data):
     """
-    Validate if the given data is UTF-8 encoded.
-
+    Determines if a given data set represents a valid UTF-8 encoding.
     Args:
-        data (list[int]): The data to be validated.
-
+        - data: list of integers representing 1 byte of data
     Returns:
-        bool: True if the data is valid UTF-8 encoded, False otherwise.
+        - True if data is a valid UTF-8 encoding, else False
     """
+    # Number of bytes in the current UTF-8 character
     num_bytes = 0
 
     for byte in data:
+        # If the current byte is the start of a new character
         if num_bytes == 0:
-            if (byte >> 5) == 0b110:
+            # Determine the number of bytes in the UTF-8 character
+            if byte >> 7 == 0b0:
                 num_bytes = 1
-            elif (byte >> 4) == 0b1110:
+            elif byte >> 5 == 0b110:
                 num_bytes = 2
-            elif (byte >> 3) == 0b11110:
+            elif byte >> 4 == 0b1110:
                 num_bytes = 3
-            elif (byte >> 7) != 0:
+            elif byte >> 3 == 0b11110:
+                num_bytes = 4
+            else:
                 return False
         else:
-            if (byte >> 6) != 0b10:
+            # Check if the current byte is a continuation byte
+            if byte >> 6 != 0b10:
                 return False
+
+            # Decrement the number of bytes expected for the current character
             num_bytes -= 1
 
+    # If there are remaining bytes to complete a character, it's invalid
     return num_bytes == 0
-
-
-if __name__ == "__main__":
-    pass
