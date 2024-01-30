4da52d35d084f837d907f764e19967aa067e502a
sabrallah youssef <58825588+sabrallah@users.noreply.github.com>
2024-01-30 19:42:47 +0000

    task done

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,1 +1,28 @@
-y
+#!/usr/bin/python3
+
+def validUTF8(data):
+    """
+    Check if the given data is a valid UTF-8 encoding.
+
+    Args:
+        data (list[int]): The data to be checked.
+
+    Returns:
+        bool: True if the data is a valid UTF-8 encoding, False otherwise.
+    """
+    num_bytes = 0
+    for num in data:
+        if num_bytes == 0:
+            if (num >> 5) == 0b110:
+                num_bytes = 1
+            elif (num >> 4) == 0b1110:
+                num_bytes = 2
+            elif (num >> 3) == 0b11110:
+                num_bytes = 3
+            elif (num >> 7):
+                return False
+        else:
+            if (num >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+    return num_bytes == 0
