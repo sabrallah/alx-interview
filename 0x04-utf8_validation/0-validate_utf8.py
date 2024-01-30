78a27e77fb5f0a128579bb31f70b9a0a6ee436d9
sabrallah youssef <58825588+sabrallah@users.noreply.github.com>
2024-01-30 20:02:24 +0000

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 """
 This module contains a function for validating UTF-8 encoded data.
 """
@@ -15,12 +13,24 @@ def validUTF8(data):
     Returns:
         bool: True if the data is valid UTF-8 encoded, False otherwise.
     """
-    try:
-        bytes_data = bytes(data)
-        bytes_data.decode('utf-8')
-        return True
-    except UnicodeDecodeError:
-        return False
+    num_bytes = 0
+
+    for byte in data:
+        if num_bytes == 0:
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7) != 0:
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
 
 
 if __name__ == "__main__":
