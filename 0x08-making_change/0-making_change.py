5c301e6d32b36be2a75fd7df05f5d1134f39abd7
sabrallah youssef <sabrallah1991@gmail.com>
2024-02-27 09:07:25 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """
-this module solve change problem 
+This module solves the change problem
 """
 
 
-def makeChange(coins, total):
+def make_change(coins, total):
     """
-    the few number coins need meet the total
+    Finds the fewest number of coins needed to meet total
     """
     if total <= 0:
         return 0
     coin_min = {0: 0}
     for value in range(1, total + 1):
         coin_min[value] = float('inf')
-        for Coin in coins:
-            if value >= Coin:
-                coin_min[value] = min(coin_min[value],
-                                        coin_min[value - Coin] + 1)
+        for coin in coins:
+            if value >= coin:
+                coin_min[value] = min(coin_min[value], coin_min[value - coin] + 1)
     if coin_min[total] != float('inf'):
         return coin_min[total]
     else:
