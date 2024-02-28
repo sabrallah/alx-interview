2020490fe0321a5b70690680e3b0f5ef4d5e4b5b
sabrallah youssef <sabrallah1991@gmail.com>
2024-02-28 20:21:50 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,37 +1,6 @@
 #!/usr/bin/python3
 """
 Making Change problem
-
-https://www.algoexpert.io/questions/Making%20Change
-
-Given an infinite number of quarters (25 cents), dimes (10 cents),
-nickels (5 cents), and pennies (1 cent), write code to calculate the
-number of ways of making n cents.
-
-Constraints:
-0 <= n <= 10000
-
-Example:
-n = 4
-
-Number of ways of making 4 cents is 4. The ways are:
-1 cent, 1 cent, 1 cent, 1 cent
-1 cent, 1 cent, 2 pennies
-1 cent, 2 nickels
-2 pennies, 2 pennies
-
-Assumptions:
-- Coins have infinite supply
-
-Algorithm:
-Dynamic programming
-
-Time Complexity:
-O(n)
-
-Space Complexity:
-O(n)
-
 """
 
 
